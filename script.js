9f4ab2ef937cbbafb61cba0960268b5b279a4c7c
Pawel233445 <testpawel421@gmail.com>
2025-01-03 00:25:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,27 @@
 let threadId = null;
 let messageCount = 0;
-let welcomeMessageShown = false;
 const chatContainer = document.getElementById('chat-container');
-const chatInput = document.querySelector('#chat-input-frame input');
+const chatInput = document.querySelector('#chat-input input');
 const chatSendButton = document.querySelector('#chat-input button');
 const chatMessages = document.getElementById('chat-messages');
 const resetChat = document.getElementById('reset-chat');
-const resetButtonMobile = document.getElementById('reset'); // Referencja do nowego przycisku resetowania
-const SERVERLESS_FUNCTION_URL = 'https://europe-central2-mickiewicz.cloudfunctions.net/chat';
 const chatForm = document.getElementById('chat-form');
-const backButton = document.getElementById('back-button');
-const leftSide = document.querySelector('.left-side');
-const mobileWelcomeScreen = document.querySelector('.mobile-welcome-screen');
-const chatWrapper = document.getElementById('chat-wrapper');
-const startChatButton = document.getElementById('start-chat-button');
+const textContainer = document.querySelector('.text-container'); // Dodane - pobieranie kontenera z tekstem
+const SERVERLESS_FUNCTION_URL = 'https://europe-central2-mickiewicz.cloudfunctions.net/chat'; // Zmień na URL swojej funkcji
 
-function saveChatToLocalStorage() {
-    const chatData = {
-        messages: Array.from(chatMessages.children).map(element => ({
-            role: element.classList.contains('user-message') ? 'user' : 'assistant',
-            message: element.innerHTML,
-            isMarkdown: element.innerHTML.includes("<p>")
-        })),
-        threadId: threadId,
-        messageCount: messageCount
-    };
-    localStorage.setItem('chatData', JSON.stringify(chatData));
-}
-
-function loadChatFromLocalStorage() {
-    const storedChatData = localStorage.getItem('chatData');
-    if (storedChatData) {
-        const chatData = JSON.parse(storedChatData);
-        if (chatData.messages) {
-            chatData.messages.forEach(msg => {
-                displayMessage(msg.role, msg.message, msg.isMarkdown);
-            });
-        }
-        threadId = chatData.threadId;
-        messageCount = chatData.messageCount;
-        console.log('Chat loaded from local storage with thread ID:', threadId);
-        scrollToBottom();
-    }
-}
-
-async function initializeChat(showWelcomeMessage = true) {
+async function initializeChat() {
     try {
         messageCount = 0;
-        loadChatFromLocalStorage();
-         if(!threadId && showWelcomeMessage && !welcomeMessageShown) {
-            displayMessage('assistant', 'Cześć, jestem Adam Mickiewicz i chętnie Ci o sobie opowiem. :)');
-           console.log('Chat initialized with thread ID:', threadId);
-           welcomeMessageShown = true;
-        }
+        console.log('Chat initialized with thread ID:', threadId);
+        displayMessage('assistant', 'Cześć, jestem Adam Mickiewicz i chętnie Ci o sobie opowiem. :)');
     } catch (error) {
         console.error('Error initializing chat:', error);
     }
 }
 
+
 async function sendMessage(message) {
     try {
-        console.log('Wysyłanie wiadomości:', message);
         displayMessage('user', message);
         displayTypingIndicator();
         messageCount++;
@@ -75,15 +36,12 @@ async function sendMessage(message) {
                 threadId: threadId
             }),
         });
-        console.log('Odpowiedź serwera:', response);
         const data = await response.json();
         removeTypingIndicator();
-        console.log('Dane z serwera:', data);
         if (data.threadId) {
-            threadId = data.threadId;
+            threadId = data.threadId
         }
         displayMessage('assistant', data.response, true);
-        saveChatToLocalStorage();
     } catch (error) {
         console.error('Error sending message:', error);
         removeTypingIndicator();
@@ -104,6 +62,7 @@ function displayMessage(role, message, isMarkdown = false) {
     scrollToBottom();
 }
 
+
 function scrollToBottom() {
     const lastMessage = chatMessages.lastElementChild;
     if (lastMessage) {
@@ -113,7 +72,9 @@ function scrollToBottom() {
             chatMessages.scrollTop = lastMessageBottom - containerHeight;
         }
     }
+
 }
+
 
 function displayTypingIndicator() {
     const typingIndicator = document.createElement('div');
@@ -130,70 +91,53 @@ function removeTypingIndicator() {
     }
 }
 
-function clearChat() {
+
+// Event Listeners
+resetChat.addEventListener('click', () => {
+    // Reset all counters when closing the chat
     messageCount = 0;
     chatMessages.innerHTML = '';
-    threadId = null;
-    localStorage.removeItem('chatData');
-    welcomeMessageShown = false;
+    threadId = null; // ustawiamy na null
+    sessionStorage.removeItem('initialScreenShown'); // Usunięcie flagi z sessionStorage
+    if (textContainer) { // Dodane - Sprawdzenie czy textContainer istnieje
+        textContainer.style.display = ''; //  Dodane - ponowne wyświetlenie planszy wstępnej
+    }
     initializeChat();
-}
-
-resetChat.addEventListener('click', clearChat);
-
-resetButtonMobile.addEventListener('click', clearChat);
-
-chatForm.addEventListener('submit', function (event) {
+});
+    
+chatForm.addEventListener('submit', function(event) {
     event.preventDefault();
     const message = chatInput.value.trim();
-    if (message) {
+      if (message) {
         sendMessage(message);
         chatInput.value = '';
+        if(textContainer) { // Dodane - Sprawdzenie czy textContainer istnieje
+            textContainer.style.display = 'none'; // Dodane - ukrycie planszy wstępnej po wysłaniu pierwszej wiadomości
+        }
     }
 });
 
-backButton.addEventListener('click', () => {
-    mobileWelcomeScreen.style.display = 'flex';
-    chatWrapper.style.display = 'none';
-    backButton.style.display = 'none';
-    if (window.innerWidth <= 768) {
-        leftSide.style.display = 'none';
-    } else {
-        leftSide.style.display = 'flex';
+
+chatInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        chatSendButton.click();
     }
 });
 
-startChatButton.addEventListener('click', () => {
-    mobileWelcomeScreen.style.display = 'none';
-    chatWrapper.style.display = 'flex';
-    if (window.innerWidth <= 768) {
-        backButton.style.display = 'flex';
-        leftSide.style.display = 'none';
-    }
 
-});
-
+// Initialize chat on page load
 window.onload = function () {
-    if (window.innerWidth > 768) {
-        mobileWelcomeScreen.style.display = 'none';
-        chatWrapper.style.display = 'flex';
-        backButton.style.display = 'none';
-         initializeChat();
-    } else {
-           mobileWelcomeScreen.style.display = 'flex';
-          chatWrapper.style.display = 'none';
-          initializeChat(); // Przywróć wywołanie initializeChat
+    const initialScreenShown = sessionStorage.getItem('initialScreenShown');
+     if (!initialScreenShown) {
+        // Jeśli flaga nie istnieje, to pokaż planszę wstępną i ustaw flagę.
+         if (textContainer) { // Dodane - Sprawdzenie czy textContainer istnieje
+          textContainer.style.display = ''; // Dodane - pokazanie planszy wstępnej
+          sessionStorage.setItem('initialScreenShown', 'true'); // Dodane - ustawienie flagi w sessionStorage
+         }
+     } else {
+         if(textContainer) { // Dodane - Sprawdzenie czy textContainer istnieje
+            textContainer.style.display = 'none'; // Dodane - ukrycie planszy wstępnej jeśli flaga istnieje
+         }
     }
+    initializeChat();
 };
-
-window.addEventListener('resize', function () {
-    if (window.innerWidth <= 768) {
-        mobileWelcomeScreen.style.display = 'flex';
-        chatWrapper.style.display = 'none';
-        backButton.style.display = 'flex';
-    } else {
-        mobileWelcomeScreen.style.display = 'none';
-        chatWrapper.style.display = 'flex';
-        backButton.style.display = 'none';
-    }
-});
